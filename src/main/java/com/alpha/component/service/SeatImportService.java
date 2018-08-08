b3feb4922f9e16a737e49d54d331c4bf234e7dcb
Abhishek Kundalia <Abhishek.Kundalia@gfs.com>
2018-08-08 16:42:47 +0530

    implemented tgdfrfr

--- src/main/java/com/alpha/component/service/SeatImportService.java
+++ src/main/java/com/alpha/component/service/SeatImportService.java
@@ -3,27 +3,23 @@ package com.alpha.component.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.seats.arrangement.dao.SeatsArrangementRepository;
-import com.seats.arrangement.dto.SeatDTO;
+import com.alpha.component.dto.SeatDTO;
+import com.seats.component.repository.SeatsArrangementRepository;
 
 @RestController
 @RequestMapping(value = "/api/import/")
-@CrossOrigin(maxAge = 3600)
-@EnableMongoRepositories("com.seats.arrangement.dao")
 public class SeatImportService {
 
-	@Autowired
-	private SeatsArrangementRepository seatsArrangementRepository;
+    @Autowired
+    private SeatsArrangementRepository seatsArrangementRepository;
 
-	@RequestMapping(value = "/saveSeatingTemplate", method = RequestMethod.POST)
-	public void saveTemplate(@RequestBody List<SeatDTO> listOfSeatsList) {
-		seatsArrangementRepository.save(listOfSeatsList);
-	}
+    @RequestMapping(value = "/saveSeatingTemplate", method = RequestMethod.POST)
+    public void saveTemplate(@RequestBody List<SeatDTO> listOfSeatsList) {
+        seatsArrangementRepository.save(listOfSeatsList);   
+    }
 }
---
+++ src/main/java/com/seats/component/repository/SeatsArrangementRepository.java
@@ -0,0 +1,14 @@
+package com.seats.component.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.alpha.component.dto.SeatDTO;
+
+public interface SeatsArrangementRepository extends MongoRepository<SeatDTO, String> {
+
+    public List<SeatDTO> findByBayIdAndFloorAndBuildingOrderByRowIdAscColIdAsc(String bayId, String floor,
+            String building);
+
+}
