24a6077f21842d3a0c511ea949719acdfa51eab6
Akshay Sharma <akshay.sharma@gfs.com>
2018-08-08 19:55:20 +0530

    Changes made to add aservice to fetch all buildings

---
+++ src/main/java/com/alpha/component/service/BuildingRetrievalService.java
@@ -0,0 +1,57 @@
+package com.alpha.component.service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alpha.component.constants.StatusMsgCd;
+import com.alpha.component.dto.ResponseListDTO;
+import com.alpha.component.dto.SeatDTO;
+import com.seats.component.repository.SeatsArrangementRepository;
+
+@RestController
+@RequestMapping(value = "/api/buildingsRetrieval")
+public class BuildingRetrievalService {
+
+	@Autowired
+	private SeatsArrangementRepository seatsArrangementRepository;
+
+	@RequestMapping(value = "/fetchAllBuildings", method = RequestMethod.GET, produces = "application/json")
+	public ResponseListDTO<String> fetchAllRequests() {
+		ResponseListDTO<String> responseListDTO = new ResponseListDTO<>();
+		List<SeatDTO> seatDtoList = new ArrayList<>();
+		try {
+			seatDtoList = this.seatsArrangementRepository.findAll();
+			if (CollectionUtils.isNotEmpty(seatDtoList)) {
+				responseListDTO.setResults(fetchListOfBuildings(seatDtoList));
+				responseListDTO.setStatusCd(StatusMsgCd.RESULT_FOUND);
+				responseListDTO.setMsg(StatusMsgCd.RESPONSE_200);
+			} else {
+				responseListDTO.setStatusCd(StatusMsgCd.NO_RESULT_FOUND);
+				responseListDTO.setMsg(StatusMsgCd.RESPONSE_404);
+			}
+		} catch (Exception e) {
+		}
+		return responseListDTO;
+
+	}
+
+	private List<String> fetchListOfBuildings(List<SeatDTO> seatDtoList) {
+
+		Set<String> distinctBuildingNames = new HashSet<String>();
+		for (SeatDTO seatDto : seatDtoList) {
+			distinctBuildingNames.add(seatDto.getBuilding());
+		}
+
+		return new ArrayList<>(distinctBuildingNames);
+	}
+
+}
