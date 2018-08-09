bdbb87edf62f8954a0b99adec62aecd3a3312004
Abhishek Kundalia <Abhishek.Kundalia@gfs.com>
2018-08-09 11:19:22 +0530

    updated the Mongo repo package

--- src/main/java/com/organisation/seats/SeatAllocationStarterApplication.java
+++ src/main/java/com/organisation/seats/SeatAllocationStarterApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableMongoRepositories("com.seats.component.repository")
+@EnableMongoRepositories("com.organisation.seats.component.repository")
 public class SeatAllocationStarterApplication {
 
     public static void main(String[] args) {
