068b940a20f931c8909d847681007b1516426731
Abhishek Kundalia <Abhishek.Kundalia@gfs.com>
2018-08-09 18:59:57 +0530

    Request Approval service implemented

--- src/main/java/com/organisation/seats/component/constants/StatusMsgCd.java
+++ src/main/java/com/organisation/seats/component/constants/StatusMsgCd.java
@@ -2,12 +2,12 @@ package com.organisation.seats.component.constants;
 
 public class StatusMsgCd {
 
-    public static final Integer RESULT_FOUND = 200;
+    public static final Integer RESPONSE_200 = 200;
 
-    public static final String RESPONSE_200 = "Results Found";
+    public static final String RESULT_FOUND = "Results Found";
 
-    public static final Integer NO_RESULT_FOUND = 404;
+    public static final Integer RESPONSE_404 = 404;
 
-    public static final String RESPONSE_404 = "Results Not Found";
+    public static final String NO_RESULT_FOUND = "Results Not Found";
 
 }
