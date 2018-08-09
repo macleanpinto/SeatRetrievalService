880fa297ac08631e3c6f318199177bc2bc28d24f
Digvijay Pratap Singh <DigvijayPratap.Singh@gfs.com>
2018-08-09 22:30:51 +0530

    floor retrieval

--- src/main/java/com/alpha/component/service/FloorRetrievalService.java
+++ src/main/java/com/alpha/component/service/FloorRetrievalService.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alpha.component.constants.StatusMsgCd;
-import com.alpha.component.dto.ResponseListDTO;
-import com.alpha.component.dto.SeatDTO;
-import com.seats.component.repository.SeatsArrangementRepository;
+import com.organisation.seats.component.constants.StatusMsgCd;
+import com.organisation.seats.component.dto.ResponseListDTO;
+import com.organisation.seats.component.dto.SeatDTO;
+import com.organisation.seats.component.repository.SeatsArrangementRepository;
+
+
 
 @RestController
 @RequestMapping(value = "/api/floorRetrieval")
